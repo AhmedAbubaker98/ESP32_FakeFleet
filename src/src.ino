864927860bd45c0f0ddc8ffb835101646a24ac66
Ahmed Abubekr <ahmedkata@gmail.com>
2024-12-02 17:04:14 +0300

    added gitignore, fixed secrets.h, added bin file

--- src/src.ino
+++ src/src.ino
@@ -4,6 +4,9 @@
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include "ESP32OTAPull.h"
+#define JSON_URL   "https://github.com/AhmedAbubaker98/ESP32_FakeFleet/tree/main/src/version_control.json"
+#define VERSION    "0.3" // The current version of this program
 
 #define AWS_IOT_PUBLISH_TOPIC   "ESP32Thing/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "ESP32Thing/sub"
@@ -88,13 +91,84 @@ void setup()
   setupOTA("ESP32_FakeFleet", WIFI_SSID, WIFI_PASSWORD);
 
   connectWiFiAWS();
+
+  ESP32OTAPull ota;
+
+  ota.SetCallback(callback);
+	Serial.printf("We are running version %s of the sketch, Board='%s', Device='%s'.\n", VERSION, ARDUINO_BOARD, WiFi.macAddress().c_str());
+	Serial.printf("Checking %s to see if an update is available...\n", JSON_URL);
+	int ret = ota.CheckForOTAUpdate(JSON_URL, VERSION);
+	Serial.printf("CheckForOTAUpdate returned %d (%s)\n\n", ret, errtext(ret));
 }
  
 void loop()
 {
-  ArduinoOTA.handle();
  
   publishMessage();
+  Serial.println("Sent!");
   client.loop();
   delay(2000);
+}
+
+const char *errtext(int code)
+{
+	switch(code)
+	{
+		case ESP32OTAPull::UPDATE_AVAILABLE:
+			return "An update is available but wasn't installed";
+		case ESP32OTAPull::NO_UPDATE_PROFILE_FOUND:
+			return "No profile matches";
+		case ESP32OTAPull::NO_UPDATE_AVAILABLE:
+			return "Profile matched, but update not applicable";
+		case ESP32OTAPull::UPDATE_OK:
+			return "An update was done, but no reboot";
+		case ESP32OTAPull::HTTP_FAILED:
+			return "HTTP GET failure";
+		case ESP32OTAPull::WRITE_ERROR:
+			return "Write error";
+		case ESP32OTAPull::JSON_PROBLEM:
+			return "Invalid JSON";
+		case ESP32OTAPull::OTA_UPDATE_FAIL:
+			return "Update fail (no OTA partition?)";
+		default:
+			if (code > 0)
+				return "Unexpected HTTP response code";
+			break;
+	}
+	return "Unknown error";
+}
+
+
+void DisplayInfo()
+{
+	char exampleImageURL[256];
+	snprintf(exampleImageURL, sizeof(exampleImageURL), "https://example.com/Basic-OTA-Example-%s-%s.bin", ARDUINO_BOARD, VERSION);
+
+	Serial.printf("Basic-OTA-Example v%s\n", VERSION);
+	Serial.printf("You need to post a JSON (text) file similar to this:\n");
+	Serial.printf("{\n");
+	Serial.printf("  \"Configurations\": [\n");
+	Serial.printf("    {\n");
+	Serial.printf("      \"Board\": \"%s\",\n", ARDUINO_BOARD);
+	Serial.printf("      \"Device\": \"%s\",\n", WiFi.macAddress().c_str());
+	Serial.printf("      \"Version\": \"%s\",\n", VERSION);
+	Serial.printf("      \"URL\": \"%s\"\n", exampleImageURL);
+	Serial.printf("    }\n");
+	Serial.printf("  ]\n");
+	Serial.printf("}\n");
+	Serial.printf("\n");
+	Serial.printf("(Board, Device, Config, and Version are all *optional*.)\n");
+	Serial.printf("\n");
+	Serial.printf("Post the JSON at, e.g., %s\n", JSON_URL);
+	Serial.printf("Post the compiled bin at, e.g., %s\n\n", exampleImageURL);
+}
+
+void callback(int offset, int totallength)
+{
+	Serial.printf("Updating %d of %d (%02d%%)...\n", offset, totallength, 100 * offset / totallength);
+  #if defined(LED_BUILTIN) // flicker LED on update
+	static int status = LOW;
+	status = status == LOW && offset < totallength ? HIGH : LOW;
+	digitalWrite(LED_BUILTIN, status);
+  #endif
 }
